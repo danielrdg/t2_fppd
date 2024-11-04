c13a5aaaf38ed4c100033434f52943e1deaa9177
Daniel Araujo <daniel.a001@edu.pucrs.br>
2024-11-03 22:14:08 -0300

    fix registry para ser criado quando o MainServidor for executado

--- src/MainServidor.java
+++ src/MainServidor.java
@@ -4,9 +4,15 @@ import java.rmi.registry.Registry;
 public class MainServidor {
     public static void main(String[] args) {
         try {
+            // Cria o registry na porta 1099 (ou usa o existente)
+            Registry registry = LocateRegistry.createRegistry(1099);
+            
+            // Inicializa o objeto remoto
             ServidorInterface servidor = new Servidor();
-            Registry registry = LocateRegistry.getRegistry(1099); // Conecta ao registry já em execução
-            registry.rebind("Banco", servidor); // Registra o objeto remoto
+            
+            // Registra o objeto remoto no RMI Registry
+            registry.rebind("Banco", servidor);
+            
             System.out.println("Servidor iniciado e registrado no RMI Registry...");
         } catch (Exception e) {
             e.printStackTrace();
